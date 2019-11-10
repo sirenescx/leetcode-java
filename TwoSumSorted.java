7b4c5d2e7af525ccaacb71d25b29392ad5ba588d
= <msmanakhova@edu.hse.ru>
2019-11-10 14:58:05 +0300

    10.11.2019 14:58PM

---
+++ TwoSumSorted.java
@@ -0,0 +1,44 @@
+/* Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+ * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+ * Note:
+ * Your returned answers (both index1 and index2) are not zero-based.
+ * You may assume that each input would have exactly one solution and you may not use the same element twice.
+ * Example:
+ * Input: numbers = [2,7,11,15], target = 9
+ * Output: [1,2]
+ * Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2. */
+
+import java.util.*;
+
+class Solution {
+
+	// less memory
+    public int[] twoSum1(int[] numbers, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < numbers.length; ++i) {
+            int difference = target - numbers[i];
+            if (hashMap.containsKey(difference)) {
+                return new int[] { hashMap.get(difference), i + 1 };
+            }
+            hashMap.put(numbers[i], i + 1);
+        }
+        return null;
+    }
+
+    // faster
+    public int[] twoSum2(int[] numbers, int target) {
+    	for (int i = 0, j = numbers.length - 1, sum = 0 ; i < numbers.length ;)
+        {
+            sum = numbers[i] + numbers[j];
+            if (sum == target)
+                return new int[] { i + 1, j + 1 };
+            if (sum < target) {
+                ++i;
+            } else {
+                --j;
+            }
+        }
+        return null;
+    }
+
+}
