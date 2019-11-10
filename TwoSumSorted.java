c0cc39ea55269bd7e9f4e57a592539ad0e245071
Maria Manakhova <47086248+sirenescx@users.noreply.github.com>
2019-11-10 15:04:06 +0300

    Update TwoSumSorted.java

--- TwoSumSorted.java
+++ TwoSumSorted.java
@@ -12,7 +12,6 @@ import java.util.*;
 
 class Solution {
 
-    // less memory
     public int[] twoSum1(int[] numbers, int target) {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < numbers.length; ++i) {
