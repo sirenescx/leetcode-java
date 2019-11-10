bc2fbee1949be59d984a3fe139ec0dca13c0bd60
Maria Manakhova <47086248+sirenescx@users.noreply.github.com>
2019-11-10 15:05:53 +0300

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -11,7 +11,7 @@ class Solution {
     public int[] twoSum1(int[] nums, int target) {
         int[] indices = new int[2];
         for (int i = 0; i < nums.length - 1; ++i) {
-            for (int j = i + 1; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; ++j) {
                 if (nums[i] + nums[j] == target) {
                     indices[0] = i;
                     indices[1] = j;
