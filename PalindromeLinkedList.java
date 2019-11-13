e2e58ed1d07d51ecb7ee73a2e45fd4d97198d929
= <msmanakhova@edu.hse.ru>
2019-11-13 19:45:02 +0300

    13.11.2019 7:44 PM

--- PalindromeLinkedList.java
+++ PalindromeLinkedList.java
@@ -47,4 +47,40 @@ class Solution {
         Collections.reverse(rList);
         return Objects.equals(rList, list);
     }
+
+    public boolean isPalindrome3(ListNode head) {
+        if (head == null || head.next == null) return true;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode newHead = slow.next;
+        slow.next = null;
+        
+        ListNode pre = null;
+        ListNode curr = newHead;
+        
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        
+        ListNode p1 = head;
+        ListNode p2 = pre;
+        
+        while (p1 != null && p2 != null) {
+            if (p1.val != p2.val) return false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        
+        return true;
+    }
 }
